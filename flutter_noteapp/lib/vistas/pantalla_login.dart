26a18a5b60e79b8cb1186d9ed879967dd42bda12
Ashly <avct2001@gmail.com>
2023-04-10 15:19:02 -0400

    Parte Pantalla Login

--- flutter_noteapp/lib/vistas/pantalla_login.dart
+++ flutter_noteapp/lib/vistas/pantalla_login.dart
@@ -24,24 +24,59 @@ class _PantallaLoginState extends State<PantallaLogin> {
         ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding:const EdgeInsets.only(top: 110.0),
-          
-          child: Column(
-            children: <Widget> [
-              //Imagen principal
-              Image.asset("assets/noteappd.png",
-                width: 220,
-                fit: BoxFit.cover,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:const EdgeInsets.only(top: 110.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: <Widget> [
+                    //Imagen principal
+                    Image.asset("assets/noteappd.png",
+                      width: 220,
+                      fit: BoxFit.cover,
+                    ),
+                    const Spacer(),
+                    //Imagen Nombre App
+                    Image.asset("assets/notasappcolor.png",
+                      width: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
               ),
-              //const Spacer(),
-              //Imagen Nombre App
-              Image.asset("assets/notasappcolor.png",
-                width: 150,
-                fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 40),
+            /* Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
               ),
-            ],
-          ),
+              child: Container(
+                text
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFFFFF),
+                  border: Border.all(
+                    color: const Color(0XFC0C0C0),
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Usuario o Correo',
+                      hintStyle: TextStyle(
+                        color: Color(0x00000000),
+                      )
+                    ),
+                  ),
+                )
+              ),
+            ), */
+          ],
         ),
       ),
     );

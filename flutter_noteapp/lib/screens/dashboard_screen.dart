bcb95a404c6f31b74a556599ea9993ee04a626b7
omar.lopez <contacto.omarlopez@gmail.com>
2023-04-11 16:15:10 -0400

    add Menu Tab

--- flutter_noteapp/lib/screens/dashboard_screen.dart
+++ flutter_noteapp/lib/screens/dashboard_screen.dart
@@ -10,52 +10,135 @@ class DashboardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppTheme.bgGray,
-        elevation: 0,
-        title: Image.asset("assets/NotasAppColor.png",width: 100,),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            // Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.menu, color: AppTheme.text_dark ),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search, color: AppTheme.text_dark),
+     return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppTheme.bgGray,
+          elevation: 0,
+          title: Image.asset("assets/NotasAppColor.png",width: 100,),
+          centerTitle: true,
+          leading: IconButton(
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
+                    const SnackBar(content: Text('Menu Proximamente')));
             },
+            icon: const Icon(Icons.menu, color: AppTheme.text_dark ),
           ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search, color: AppTheme.text_dark),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Buscar Proximamente')));
+              },
+            ),
+            ],
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Todos',
+              ),
+              Tab(
+                text: 'Universidad',
+              ),
+              Tab(
+                text: 'Trabajo',
+              ),
+              Tab(
+                text: 'Hogar',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Column(children: const [ 
+                userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_1), 
+                userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_2), 
+                userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_2), 
+                userNotes(noteText:'sfg'     ,date:'date', color:AppTheme.note_3), 
+                userNotes(noteText:'kkkk'    ,date:'date', color:AppTheme.note_3), 
+             ], 
+            ),
+            Column(children: const [ 
+                userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_1),  
+             ], 
+            ),
+             Column(children: const [ 
+                userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_2), 
+                userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_2), 
+                 
+             ], 
+            ),
+             Column(children: const [ 
+                userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_3), 
+                userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_3), 
+               
+             ], 
+            ),
           ],
-
-      ),
-      body: 
-      
-      Center(child: Column(
-        children: const [  
-          userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_1), 
-          userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_2), 
-          userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_3), 
-          userNotes(noteText:'sfg'     ,date:'date', color:AppTheme.note_4), 
-          userNotes(noteText:'kkkk'    ,date:'date', color:AppTheme.note_5), 
-
-        ],
-      ) 
-      ),
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: AppTheme.primary,
-        child: const Icon(Icons.add, color: AppTheme.text_dark,),
-        elevation: 2, 
+        ),
         
-
-      ) ,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+       floatingActionButton: FloatingActionButton(
+         onPressed: () {},
+         backgroundColor: AppTheme.primary,
+         child: const Icon(Icons.add, color: AppTheme.text_dark,),
+         elevation: 2,  
+       ) ,
+       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      ),
     );
+    
   }
 }
+
+
+// return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: AppTheme.bgGray,
+//         elevation: 0,
+//         title: Image.asset("assets/NotasAppColor.png",width: 100,),
+//         centerTitle: true,
+//         leading: IconButton(
+//           onPressed: () {
+//             // Navigator.of(context).pop();
+//           },
+//           icon: const Icon(Icons.menu, color: AppTheme.text_dark ),
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.search, color: AppTheme.text_dark),
+//             onPressed: () {
+//               ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('This is a snackbar')));
+//             },
+//           ),
+//           ],
+
+//       ),
+//       body: 
+      
+//       Center(child: Column(
+//         children: const [  
+//           userNotes(noteText:'sjsjsjsj',date:'date', color:AppTheme.note_1), 
+//           userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_2), 
+//           userNotes(noteText:'sdg'     ,date:'date', color:AppTheme.note_3), 
+//           userNotes(noteText:'sfg'     ,date:'date', color:AppTheme.note_4), 
+//           userNotes(noteText:'kkkk'    ,date:'date', color:AppTheme.note_5), 
+
+//         ],
+//       ) 
+//       ),
+      
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         backgroundColor: AppTheme.primary,
+//         child: const Icon(Icons.add, color: AppTheme.text_dark,),
+//         elevation: 2, 
+        
+
+//       ) ,
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+//     );
